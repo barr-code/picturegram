a7e8bed50bafd25af1baa9b3e6f4ade80538b11c
barr-code <vb329@georgetown.edu>
2015-07-08 23:33:13 +0100

    added message + image to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708215012) do
+ActiveRecord::Schema.define(version: 20150708222057) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "message"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'Posts' do
       expect(page).to have_link 'Add a Post'
     end
   end
+
+  context 'there are posts' do
+    before do
+      Post.create(message: 'Hogwarts Express! (Glenfinnan viaduct)', image: File.open("#{Rails.root}/spec/fixtures/glenfinnan.jpg"))
+    end
+
+    scenario 'display posts' do
+      visit '/'
+      expect(page).to have_content 'Hogwarts Express'
+      expect(page).not_to have_content 'No posts yet.'
+      expect(page).to have_selector 'img'
+    end
+  end
 end
