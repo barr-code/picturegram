956b9c6633bdd4351ff0c2093cf997eb12644150
barr-code <vb329@georgetown.edu>
2016-02-14 18:18:31 +0000

    making sure only relevant post's like count is updated on click; liked posts have solid heart instead of outline

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $('.add-like').on('click', function(event){  
+  $('.add-like').on('click', function(event){
     event.preventDefault();
     var url = $(this).attr('href');
 
-    var likeCount = $('.total_likes');
+    var likeCount = $(this).siblings('.total_likes');
 
     $.post(url, function(response){
       likeCount.text(response.new_like_count);
